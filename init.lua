1e5c96c654874ba278398b11a5b4bc37e919acfd
Ben Abbett <bwabbett@gmail.com>
2025-08-29 02:47:47 -0400

    feat: added a couple more mappings for managing splits and stuff

--- init.lua
+++ init.lua
@@ -36,9 +36,17 @@ vim.keymap.set({ 'n', 'v', 'x' }, '<leader>S', ':sf #<CR>')
 ----- terminal
 vim.keymap.set('n', '<leader>t', ':below terminal<CR>i')
 
-
 vim.keymap.set('i', 'jk', '<esc>')
 
+vim.keymap.set('n', '<C-j>', '<C-w>j')
+vim.keymap.set('n', '<C-k>', '<C-w>k')
+vim.keymap.set('n', '<C-h>', '<C-w>h')
+vim.keymap.set('n', '<C-l>', '<C-w>l')
+
+vim.keymap.set('n', '<C-up>', '<C-w>+')
+vim.keymap.set('n', '<C-down>', '<C-w>-')
+vim.keymap.set('n', '<C-left>', '<C-w>>')
+vim.keymap.set('n', '<C-right>', '<C-w><')
 ---- splits
 -- vim.keymap.set('n', '<leader>p', ':vsplit<CR>')
 ---- rename (should just use 'grn' instead
