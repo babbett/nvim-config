47d5311426e29fd80ed53bf9785b2e6dfbed9de7
Ben Abbett <bwabbett@gmail.com>
2025-08-29 02:05:57 -0400

    feat: lazygit now opens as a floating window

--- init.lua
+++ init.lua
@@ -71,7 +71,7 @@ vim.pack.add({
 
 require "toggleterm".setup()
 local Terminal  = require('toggleterm.terminal').Terminal
-local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction = "float" })
 
 function _lazygit_toggle()
   lazygit:toggle()
